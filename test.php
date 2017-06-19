4ff03c4d9dfe8517fae1b46055ca860ac9c0717a
wscats <2587575267@qq.com>
2017-06-19 17:35:18 +0800

    first day

---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+	echo $_GET["name"]." say: 'Hello World'";
+?>
---
+++ 创建服务器/http.js
@@ -0,0 +1,29 @@
+//引入http内置模块
+var http = require("http");
+//引入url模块
+var url = require("url");
+//引入queryString，用于格式化url上面的参数
+var querystring = require('querystring');
+//console.log(http)
+//用createServer创建服务器
+//request请求 处理请求头 请求体
+//response相应 处理响应头 相应体
+http.createServer(function(request,response){
+	//从请求头获取url
+	console.log(request.url);
+	//把我们参数部分截取出来
+	var paramStr  = url.parse(request.url).query;
+	var param = querystring.parse(paramStr);
+	/*利用querystring方法把 name=asdasd&skill=ps&kkk=sssss => {
+		name:"asdasd",
+		skill:"ps",
+		kkk:"sss"
+	}*/
+	console.log(param["name"])
+	console.log(param["skill"])
+	//解决跨域
+	response.setHeader("Access-Control-Allow-Origin","*");
+	//相应结果显示浏览器上
+	response.end("Hello World");
+}).listen(12345)
+//端口号有范围限制0~65535
