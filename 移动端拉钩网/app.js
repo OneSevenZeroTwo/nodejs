08a8941b4be62c7f70a84145f9565d977084a486
wscats <2587575267@qq.com>
2017-06-27 10:42:45 +0800

    增加几个功能

--- 移动端拉钩网/app.js
+++ 移动端拉钩网/app.js
@@ -3,6 +3,27 @@ var bodyParser = require('body-parser')
 var app = express();
 var mysql = require("mysql");
 var connection;
+var multer = require('multer');
+/*var upload = multer({
+	//如果用这种方法上传，要手动添加文明名后缀
+	dest: 'uploads/'
+})*/
+var storage = multer.diskStorage({
+	//设置上传后文件路径，uploads文件夹手动创建。
+	destination: function(req, file, cb) {
+		cb(null, './uploads')
+	},
+	//给上传文件重命名，获取添加后缀名
+	filename: function(req, file, cb) {
+		var fileFormat = (file.originalname).split(".");
+		//给图片加上时间戳格式防止重名名
+		//比如把 abc.jpg图片切割为数组[abc,jpg],然后用数组长度-1来获取后缀名
+		cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+	}
+});
+var upload = multer({
+	storage: storage
+});
 
 function createConnection() {
 	connection = mysql.createConnection({
@@ -19,6 +40,7 @@ app.use(bodyParser.urlencoded({
 	}))
 	//设置静态文件 app.js根目录下寻找public文件夹作为静态文件夹
 app.use(express.static('public'));
+app.use(express.static('uploads'));
 // parse application/json 
 //是要get请求并且匹配到路由`/`，我就执行回调，并用`res.send`方法去相应结果
 app.get('/', function(req, res) {
@@ -77,6 +99,18 @@ app.all('/test', function(req, res) {
 	console.log(req.cookies)
 	res.send('进入到test页面');
 })
+
+ 
+//单图上传
+//app.post('/upload-single', upload.single('logo'), function(req, res, next) {
+app.post('/upload-single', upload.any(), function(req, res, next) {
+	console.log(req.files)
+	res.append("Access-Control-Allow-Origin","*");
+	res.send({
+		wscats_code: '0',
+		imgInfo: req.files
+	});
+});
 
 var server = app.listen(8081, function() {
 	//测试
